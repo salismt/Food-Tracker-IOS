81454a98676d3d3f04ed75766a8aa08f7ac3dc1b
Salis <tsalis.mohammad@gmail.com>
2016-09-20 17:18:12 +0700

    add Outlets and delegates to ViewController

---
+++ FoodTrackerSwift3/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  FoodTrackerSwift3
+//
+//  Created by Salis on 9/20/16.
+//  Copyright © 2016 Salis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default text"
+    }
+   
+    
+}
+
