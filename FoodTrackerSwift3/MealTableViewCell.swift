cd39a71d839289fa7cb5197ede924cd398147340
Salis <tsalis.mohammad@gmail.com>
2016-09-21 22:35:58 +0700

    adding table view layout

---
+++ FoodTrackerSwift3/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackerSwift3
+//
+//  Created by Salis on 9/21/16.
+//  Copyright © 2016 Salis. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
