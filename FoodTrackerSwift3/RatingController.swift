eba59881f2712867dedd58869e6e83819515fa40
Salis <tsalis.mohammad@gmail.com>
2016-09-21 14:00:57 +0700

    align layouts horizontally if new view is added

--- FoodTrackerSwift3/RatingController.swift
+++ FoodTrackerSwift3/RatingController.swift
@@ -29,6 +29,17 @@ class RatingController: UIView {
         
     }
     
+    // MARK: Initialization
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        // offset each button's origin by length of the button plus spacing
+        for (index, button) in ratingButtons.enumerated() {
+            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            button.frame = buttonFrame
+        }
+    }
+    
     // MARK: Button Action
     func ratingButtonTapped(button: UIButton) {
         print("Button pressed 👍")
